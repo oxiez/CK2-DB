20bf6e86011a7f7ab576376174a11d32f7d3699e
OwenDurkin <owen@thedurks.com>
2019-03-16 16:03:37 -0400

    update to dynasties code

--- get_dynasties.py
+++ get_dynasties.py
@@ -3,14 +3,23 @@
 def get_dynasties(file,cur):
     # read until we hit the dynasties
     line = file.readline()
-    while(line):
+    done = False
+    while(not done):
         line = file.readline()
-        
         #beginning of dynasties data
         if line=='\tdynasties\n':
-            #every dynasty has 14 lines of information
-            i = line[2:len(line-1)]
-            cur.execute('INSERT INTO dynasty Values(%s,%s)',[i,name])
-        
-        #end of dynasties data
-        if line=='\t}': break
+            #while not the end of the data
+            while line!='\t}':
+                #parse 14 lines 
+                line = line.readline()
+                #get the id
+                i = line[2:len(line-1)]
+                line = f.readline()
+                #get the name
+                name = line[2:len(line-1)]
+                #add to the database
+                cur.execute('INSERT INTO dynasty Values(%s,%s)',[i,name])
+                #ignore the next 12 lines
+                for i in range(12):
+                    line = line.readline()
+            done = True
