8ab9656973daac1a8215f68a90c083b35354402f
OwenDurkin <owen@thedurks.com>
2019-03-16 19:54:17 -0400

    update to get_chars

--- get_chars.py
+++ get_chars.py
@@ -55,13 +55,15 @@ def get_chars(file, cur):
 
 		line = file.readline()
 		line = line.strip()
+		print(num_brace)
+		print(line)
 		if line == '{':
 			num_brace += 1
 			continue
 		elif line == '}':
-			if(num_brace > 1):
-				
-				cur.execute('INSERT INTO Person Values(%s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,)', [char_id, birthName, dynasty, isMale, birthday, deathday, fatherID, real_fatherID, motherID, spouseID, religionID, cultureID, fertility, health, wealth, chostID, prestige, piety, provinceLocationID, employerID, martial, diplomacy, stewardship, intrigue, learning])
+			if(num_brace == 2):
+				print('pushing ' + str(birthName))
+				cur.execute('INSERT INTO Person Values(%s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', [char_id, birthName, dynasty, isMale, birthday, deathday, fatherID, real_fatherID, motherID, spouseID, religionID, cultureID, fertility, health, wealth, hostID, prestige, piety, provinceLocationID, employerID, martial, diplomacy, stewardship, intrigue, learning])
 				char_id = None
 				birthName = None
 				dynasty = None
@@ -96,38 +98,38 @@ def get_chars(file, cur):
 		elif (num_brace > 2):
 			continue
 		else:
-			if line[0:2] == "bn=":
-				birthName = line[3:-1]
-			elif line[0:3] == "dnt=":
-				birthName = int(line[3:])
+			if line[0:3] == "bn=":
+				birthName = line[4:-1]
+			elif line[0:4] == "dnt=":
+				birthName = int(line[4:])
 			elif line == "fem=yes":
 				isMale = False
-			elif line[0:3] == "b_d=":
-				birthday = make_date(line[4:-1])
-			elif line[0:3] == "d_d=":
-				deathday = make_date(line[4:-1])
-			elif line[0:3] == "fat=":
-				fatherID = int(line[4:])
-				real_fatherID = int(line[4:])
-			elif line[0:3] == "rfat=":
+			elif line[0:4] == "b_d=":
+				birthday = make_date(line[5:-1])
+			elif line[0:4] == "d_d=":
+				deathday = make_date(line[5:-1])
+			elif line[0:4] == "fat=":
+				fatherID = int(line[5:])
 				real_fatherID = int(line[5:])
-			elif line[0:3] == "mot=":
-				motherID = int(line[4:])
+			elif line[0:5] == "rfat=":
+				real_fatherID = int(line[6:])
+			elif line[0:4] == "mot=":
+				motherID = int(line[5:])
 			elif line[0:6] == "spouse=":
 				spouseID = int(line[7:])
 			elif line[0:3] == "rel=":
 				religionID = get_rel_ID(line[5:-1])
 			elif line[0:3] == "cul=":
 				cultureID = get_cul_ID(line[5:-1]) 
-			elif ine[0:3] == "fer=":
+			elif line[0:3] == "fer=":
 				fertility = float(line[4:]) 
-			elif ine[0:6] == "health=":
+			elif line[0:6] == "health=":
 				health = float(line[7:]) 
-			elif ine[0:6] == "wealth=":
+			elif line[0:6] == "wealth=":
 				wealth = float(line[7:])
-			elif ine[0:3] == "prs=":
+			elif line[0:3] == "prs=":
 				prestige = float(line[4:]) 
-			elif ine[0:5] == "piety=":
+			elif line[0:5] == "piety=":
 				piety = float(line[6:])
 			elif line[0:3] == "emp=":
 				employerID = int(line[4:])
@@ -143,8 +145,8 @@ def get_chars(file, cur):
 				intrigue = int(attributes[3])
 				learning = int(attributes[4])
 			elif line[0:3] == "tr=":
-				tr = line[3:-1]
+				tr = line[4:-1]
 				traits = tr.split()
 
-				for t in tr:
-					cur.execute('INSERT INTO traits Values(%s,%s)',[char_id,int(t)])
+				for t in traits:
+					cur.execute('INSERT INTO trait Values(%s,%s)',[char_id,int(t)])
