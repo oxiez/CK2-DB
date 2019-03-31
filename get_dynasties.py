0daf1a4e1c1c594faa052c488badc9165ec35978
OwenDurkin <owen@thedurks.com>
2019-03-31 12:11:16 -0400

    progress but still broken

--- get_dynasties.py
+++ get_dynasties.py
@@ -6,6 +6,7 @@ dynasty_regex = {'^name':None,'^culture':None,'^religion':None}
 
 def get_dynasties(file,cur):
 	#read in historical dynasties first 
+	'''
 	with io.open('00_dynasties.txt',encoding="cp1252") as f:
 		obj = ck2_parser.getCK2Obj(f,dynasty_regex)  
 		while obj != None:
@@ -24,6 +25,40 @@ def get_dynasties(file,cur):
 			cur.execute('INSERT INTO dynasty Values(%s,%s,%s,%s)',[dntID,name,cultureID,religionID])
 			print(dntID,name,cultureID,religionID)
 			obj = ck2_parser.getCK2Obj(f,dynasty_regex)
+	'''
+	
+	with io.open('00_dynasties.txt',encoding="cp1252") as f:
+		line = ' '
+		while line:
+			line = f.readline()
+			if line == '\n' or line[0]=='#': continue
+			dntID = line[0:line.find('=')].strip()
+			line = f.readline()
+			name = None
+			cultureID = None
+			religionID = None
+			while line != '}\n':
+				line = line.strip()
+				index = line.find('=')
+				if index != -1:
+					key = line[0:index].strip()
+					value = line[index+1:]
+					if '#' in value: value = value[0:value.index('#')]
+					value = value.strip().strip('"').rstrip().rstrip('"')
+					if key=='name':
+						name = value
+					if key=='culture':
+						cur.execute('SELECT cultureID FROM culture WHERE cultureName=%s',[value])
+						cultureID = cur.fetchone()[0]
+					if key=='religion' and '}' not in value:
+						cur.execute('SELECT religionID FROM religion WHERE religionName=%s',[value])
+						religionID = cur.fetchone()[0]					
+				line = f.readline()
+			#add this dynasty to the table
+			#NOTE: dynastyID 1061019 corresponds to two dynasties!!! von Hanover and Tiversti
+			if dntID=='1061019' or dntID=='105946' or dntID=='1059971': continue
+			#print(dntID,name,cultureID,religionID)
+			cur.execute('INSERT INTO dynasty Values(%s,%s,%s,%s)',[dntID,name,cultureID,religionID])
 		
 	#read in the save game dynasties 
 	ck2_parser.jumpTo(file, "^dynasties=")
