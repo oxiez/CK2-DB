1c9b344734f5314c693c0507fbc51978e0c89dca
OwenDurkin <owen@thedurks.com>
2019-04-18 19:26:32 -0400

    modified load_data.py so that it can be run using optional arguments by other programs

--- load_data.py
+++ load_data.py
@@ -12,57 +12,60 @@ import get_culture
 import get_traits
 import get_bloodline
 
-# connect and get a cursor
-conn = psycopg2.connect("dbname=ck2 user=charles password=frank")
-cur = conn.cursor()
+def load_data(filename):
+	# connect and get a cursor
+	conn = psycopg2.connect("dbname=ck2 user=charles password=frank")
+	cur = conn.cursor()
 
-# create the tables
-with open("ck2_make_table.sql") as f:
-	cur.execute(f.read())
+	# create the tables
+	with open("ck2_make_table.sql") as f:
+		cur.execute(f.read())
 
-#read in religion and culture
-print('Preloading religion...')
-get_religion.get_religion(cur)
-print('Preloading culture...')
-get_culture.get_culture(cur)
-print('Preloading traits...')
-get_traits.get_traits(cur)
+	#read in religion and culture
+	print('Preloading religion...')
+	get_religion.get_religion(cur)
+	print('Preloading culture...')
+	get_culture.get_culture(cur)
+	print('Preloading traits...')
+	get_traits.get_traits(cur)
 
-file_name = "Leon1067_02_12.ck2"
+	with io.open(filename, "rb") as f:
+		label = f.read(2) # Get byte object
+		if label == b"PK": # I don't know if the beginning is just PK or PK3
+			print("This file is a zip-compressed file, exiting...")
+			quit()
+		elif label == b"CK": # If PK3, then we should check CK2
+			print("Reading CK2 file...")
+		else: # Ironman mode files???
+			print("Unknown file type, exiting...")
+			quit()
 
-if len(sys.argv) > 1:
-	file_name = sys.argv[1]
+	# parse the file and fill the tables with data
+	with io.open(file_name, encoding="cp1252") as f:
+		print('Getting dynasties...')
+		get_dynasties.get_dynasties(f, cur)
+		print('Getting characters...')
+		get_chars.get_chars(f, cur)
+		print('Getting religions...')
+		get_religion.get_heresies(f, cur)
+		print('Getting provinces...')
+		get_provs.get_provs(f, cur)
+		print("Getting titles...")
+		get_titles.get_titles(f, cur)
+		# Handle case for no dlc?
+		print("Getting bloodlines...")
+		get_bloodline.get_bloodlines(f, cur)
 
-with io.open(file_name, "rb") as f:
-	label = f.read(2) # Get byte object
-	if label == b"PK": # I don't know if the beginning is just PK or PK3
-		print("This file is a zip-compressed file, exiting...")
-		quit()
-	elif label == b"CK": # If PK3, then we should check CK2
-		print("Reading CK2 file...")
-	else: # Ironman mode files???
-		print("Unknown file type, exiting...")
-		quit()
+	# commit changes made and disconnect from database
+	conn.commit()
+	cur.close()
+	conn.close()
+	
+	print("All done!")
 
-# parse the file and fill the tables with data
-with io.open(file_name, encoding="cp1252") as f:
-	print('Getting dynasties...')
-	get_dynasties.get_dynasties(f, cur)
-	print('Getting characters...')
-	get_chars.get_chars(f, cur)
-	print('Getting religions...')
-	get_religion.get_heresies(f, cur)
-	print('Getting provinces...')
-	get_provs.get_provs(f, cur)
-	print("Getting titles...")
-	get_titles.get_titles(f, cur)
-	# Handle case for no dlc?
-	print("Getting bloodlines...")
-	get_bloodline.get_bloodlines(f, cur)
 
-# commit changes made and disconnect from database
-conn.commit()
-cur.close()
-conn.close()
-
-print("All done!")
+if __name__=='__main__':
+	file_name = "Leon1067_02_12.ck2"
+	if len(sys.argv) > 1:
+		file_name = sys.argv[1]
+	load_data(file_name)
