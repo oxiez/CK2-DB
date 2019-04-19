b0915975b054e1bb2055833a63be29df57c2c452
OwenDurkin <owen@thedurks.com>
2019-04-18 20:02:08 -0400

    began framework for database.py

--- database.py
+++ database.py
@@ -1,4 +1,19 @@
+import psycopg2
+import io
+import sys
 import load_data
 
+# use load_data to fill the database with info from this game
 def setup(filename):
     load_data.load_data(filename)
+
+
+# query for specific person
+def query_person(name):
+    conn = psycopg2.connect("dbname=ck2 user=charles password=frank")
+    cur = conn.cursor()
+    
+    #code here
+    
+    cur.close()
+    conn.close()    
