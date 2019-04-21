7dced3cb9f0ae86b22217e5255e9ffb4d474f48a
OwenDurkin <owen@thedurks.com>
2019-04-21 16:00:49 -0400

    working on improved help; renamed database variable to data to avoid conflict with python filename

--- application.py
+++ application.py
@@ -3,9 +3,9 @@ import sys
 import texttable
 
 # loads the given file name using a database object
-def load_file(file_name,database):
+def load_file(file_name,data):
     print('Opening the file '+file_name)
-    database.setup(file_name)
+    data.setup(file_name)
     print('\n')
 
 #how many rows every query will return
@@ -28,13 +28,13 @@ def table_print(data,headings=None):
 #main function
 if __name__=='__main__':
     #object that maintains connection to database and performs queries
-    database = database.Data()
+    data = database.Data()
     
     #load data
     print('\nWelcome to the Database Systems Project\n')
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
-        load_file(file_name,database)
+        load_file(file_name,data)
     
     # main program loop (take commands until quitting)
     print("Type 'help' for help with commands, and 'quit' to exit.")
@@ -47,34 +47,119 @@ if __name__=='__main__':
         words = command.lower().split()
 
         if words[0] == 'help':
-            print('Commands:')
-            print(' : dynasty <args> [displays information on dynasties]')
-            print(' : title <args> [displays information on titles]')
-            print(' : person <args> [displays information on characters]')
-            print(' : religion <arg> [displays information on religions]')
-            print(' : culture <arg> [displays information on cultures]')
-            print(' : bloodline <arg> [displays information on bloodlines]')
-            print(' : bloodline_members <ID> [displays characters with bloodline of ID ID]')
-            print(' : help [displays this text]')
-            print(' : num_results <NUM> [changes the number of results displayed to NUM]')
-            print(' : load <FILENAME> [loads a file]')
-            print(' : quit [exits the program]')
-
-
+            if len(words)==1:
+                print('Commands:')
+                print(' : dynasty <args> [displays information on dynasties]')
+                print(' : title <args> [displays information on titles]')
+                print(' : person <args> [displays information on characters]')
+                print(' : religion <arg> [displays information on religions]')
+                print(' : culture <arg> [displays information on cultures]')
+                print(' : bloodline <arg> [displays information on bloodlines]')
+                print(' : bloodline_members <ID> [displays characters with bloodline of ID ID]')
+                print(' : help [displays this text]')
+                print(' : num_results <NUM> [changes the number of results displayed to NUM]')
+                print(' : load <FILENAME> [loads a file]')
+                print(' : quit [exits the program]')
+            elif len(words) > 2:
+                print("Too many arguments. Try 'help' or 'help <arg>' for more information on a type of command.")
+            else:
+                #help with a specific argument type
+                comm = words[1]
+                if comm=='dynasty':
+                    print(
+"""The dynasty command is used to explore the different dynsaties of the game.
+On its own, the command 'dynasty' will list some of the dynasties in the game.
+Optionally, we can add arguments to match the name, religion, or culture.
+For example, if we wanted to find catholic, frankish dynasties,
+we can enter 'dynasty religion catholic culture frankish'.
+The 'orderby' argument can be followed by one of prestige, piety, wealth, and count (number of members).
+This will sort the results of the search by this term, descending from greatest to least.
+To build on our previous example, if we wanted to sort by prestige, we can write
+'dynasty orderby prestige religion catholic culture frankish'."""
+                    )
+                elif comm=='title':
+                    print("""\
+The title command can be used in three ways:            e.g.
+ - to find the titles owned by a specific character     title 612
+ - to find the history of rulers of a given title       title ruler k_france
+ - to find the current holder of a given title          title current k_france"""
+                        )
+                elif comm=='person':
+                    print("""\
+The person command is very complicated."""
+                          )
+                elif comm=='religion':
+                    print("""\
+The 'religion' command on its own lists the religions in the game lexicographically.
+We have three choices for an optional argument:
+ - 'religion allmembers'   sorts religions by how many characters (throughout history) have this relgion
+ - 'religion alivemembers' sorts religions by how many living characters have this religion
+ - 'religion provinces'    sorts religions by how many on-map provinces have this culture"""
+                          )
+                elif comm=='culture':
+                    print("""\
+The 'culture' command on its own lists the cultures in the game lexicographically.
+We have three choices for an optional argument:
+ - 'culture allmembers'   sorts cultures by how many characters (throughout history) have this culture
+ - 'culture alivemembers' sorts cultures by how many living characters have this culture
+ - 'culture provinces'    sorts cultures by how many on-map provinces they have"""
+                          )
+                elif comm=='bloodline':
+                    print("""\
+The 'culture' command on its own lists the cultures in the game lexicographically.
+We have three choices for an optional argument:
+ - 'culture allmembers'   sorts cultures by how many characters (throughout history) have this culture
+ - 'culture alivemembers' sorts cultures by how many living characters have this culture
+ - 'culture provinces'    sorts cultures by how many on-map provinces they have"""
+                          )
+                elif comm=='bloodline_members':
+                    pass
+                elif comm=='help':
+                    print("""\
+The help command (attempts) to display helpful information about how to use this application."""
+                          )
+                elif comm=='num_results':
+                    print("""\
+The num_results command is used to restrict the number of results that a command can return.
+For example, if we wanted to restrict the number of rows from our next command to ten,
+we can enter the command 'num_results 10'."""
+                          )
+                elif comm=='load':
+                    print("""\
+The load command is used to load in data from a save file.
+For example, 'load leon1067_02_12.ck2' will use the data from that save game."""
+                          )
+                elif comm=='quit':
+                    print("""\
+Exits the program. Can also use 'q' or 'exit'.""")
+                
+                
+                
+                                                
         elif words[0] in {'q','quit','exit'}:
             break
 
+        #load data from another file
         elif words[0] == 'load' :
             if(len(words) != 2):
                 print('ERROR load takes one argument and one argument only')
             else:
-                load_file(words[1],database)
-
+                #attempt at error handling here could be improved
+                try:
+                    load_file(words[1],data)
+                except:
+                    print('ERROR: issue loading file ' + words[1]+'.')
+                    data = database.Data()
+        #set number of rows
         elif words[0] == 'num_results' :
             if(len(words) != 2):
                 print('ERROR num_results takes one argument and one argument only')
             else:
-                ROW_COUNT = int(words[1])
+                try:
+                    ROW_COUNT = int(words[1])
+                    print('Number of rows set to ' + str(ROW_COUNT))
+                except:
+                    print("ERROR: num_results must take a number as its argument")
         
         #dynasty queries  
         elif words[0]=='dynasty':
@@ -110,7 +195,7 @@ if __name__=='__main__':
                 i += 2
             #get person with these conditions
             if valid:
-                query_result = database.query_dynasty(query_args,query_arg_vals)
+                query_result = data.query_dynasty(query_args,query_arg_vals)
                 table = texttable.Texttable()
                 table.set_max_width(210)
                 table_print(query_result)
@@ -125,17 +210,17 @@ if __name__=='__main__':
  
             # title id returns all titles of the given personid
             elif len(words)==2:
-                query_result = database.query_title(words[1])
+                query_result = data.query_title(words[1])
                 headings = ['Person ID', 'Name', 'Dynasty', 'Title Name', 'Title ID']
             # we are looking for personID(s) given a certain title
             elif len(words)==3:
                 #rulers
                 if words[1]=='rulers':
-                    query_result = database.query_rulers(words[2])
+                    query_result = data.query_rulers(words[2])
                     headings = ['personid','Name','Dynasty','Date of Birth','Date of Death']
                 #current
                 elif words[1]=='current':
-                    query_result = database.query_ruler(words[2])
+                    query_result = data.query_ruler(words[2])
                     headings = ['personid','Name','Dynasty']
                 else:
                     print("Queries should be of the form 'title (rulers|current) titleid.'")
@@ -168,7 +253,7 @@ if __name__=='__main__':
             if valid:
                 table = texttable.Texttable()
                 headings = ['ID', 'Name', 'Dynasty', 'is Male', 'Birthday', 'Deathday', 'Father', 'Real Father', 'Mother', 'Religion', 'Culture', 'Fertility', 'Health', 'Wealth', 'Prestige', 'Piety']
-                query_result = database.query_person(query_args,query_arg_vals)
+                query_result = data.query_person(query_args,query_arg_vals)
                 table_print(query_result,headings)
         
         #bio query: gets the full information on a person
@@ -176,7 +261,7 @@ if __name__=='__main__':
             if len(words)!=2:
                 print('ERROR: query should be of the form bio personID.')
                 continue
-            query_result = database.query_personid(words[1])
+            query_result = data.query_personid(words[1])
             if len(query_result)==0:
                 print('Could not find person with personID ' + words[1])
             else:
@@ -187,20 +272,20 @@ if __name__=='__main__':
                 print('Born: ' + str(birthday) + ('' if deathday == None else '    Died: '+str(deathday)))
                 # parents
                 if fatherid!=None:
-                    father_query = database.query_personid(fatherid)[0]
+                    father_query = data.query_personid(fatherid)[0]
                     print('Father: ' + str(father_query[1]) + ' ' + ('' if father_query[2] == None else str(father_query[2])+' ') + str(fatherid))
                 if fatherid!=real_fatherid:
-                    father_query = database.query_personid(real_fatherid)[0]
+                    father_query = data.query_personid(real_fatherid)[0]
                     print('Real Father: ' + str(father_query[1]) + ' ' + ('' if father_query[2] == None else str(father_query[2])+' ') + str(real_fatherid))
                 if motherid!=None:
-                    mother_query = database.query_personid(motherid)[0]
+                    mother_query = data.query_personid(motherid)[0]
                     print('Mother: '  + str(mother_query[1]) + ' ' + ('' if mother_query[2] == None else str(mother_query[2])+ ' ') + str(motherid))
                 #titles
-                titles = database.query_title(personid)
+                titles = data.query_title(personid)
                 if len(titles)!=0:
                     print('Titles: ' + ', '.join([x[4] for x in titles]))
                 #claims
-                claims = database.query_claim(personid)
+                claims = data.query_claim(personid)
                 if len(claims)!=0:
                     print('Claims: ' + ', '.join([x[0] for x in claims]))
                 #personal info
@@ -208,13 +293,13 @@ if __name__=='__main__':
                 if deathday == None:
                     print('Prestige: ' + str(prestige) + '    Piety: ' + str(piety) + '    Health: ' + str(health) + '    Wealth: ' + str(wealth) + '    Fertility: ' + str(fertility))
                 print('Attributes: martial ' + str(martial) + ', diplomacy ' + str(diplomacy) + ', stewardship ' + str(stewardship) + ', intrigue ' + str(intrigue) + ', learning ' + str(learning))
-                print('Traits: ' + ', '.join([x[0] for x in database.query_traits(words[1])]))
+                print('Traits: ' + ', '.join([x[0] for x in data.query_traits(words[1])]))
                 
         #religion
         elif words[0]=='religion':
             if len(words) == 1:
                 headings = ['Religion']
-                query_result = database.query_religion()
+                query_result = data.query_religion()
             elif len(words) == 2:
                 headings = ['Religion', 'Number']
                 valid_args = {'allmembers','alivemembers','provinces'}
@@ -222,7 +307,7 @@ if __name__=='__main__':
                     print('Invalid argument. Try one of:')
                     print(valid_args)
                     continue
-                query_result = database.query_religion(words[1])
+                query_result = data.query_religion(words[1])
             else:
                 print('ERROR: Too many arguments.')
                 continue
@@ -233,7 +318,7 @@ if __name__=='__main__':
         elif words[0]=='culture':
             if len(words) == 1:
                 headings = ['Culture']
-                query_result = database.query_culture()
+                query_result = data.query_culture()
             elif len(words) == 2:
                 headings = ['Culture', 'Number']
                 valid_args = {'allmembers','alivemembers','provinces'}
@@ -241,7 +326,7 @@ if __name__=='__main__':
                     print('Invalid argument. Try one of:')
                     print(valid_args)
                     continue
-                query_result = database.query_culture(words[1])
+                query_result = data.query_culture(words[1])
             else:
                 print('ERROR: Too many arguments.')
                 continue
@@ -268,7 +353,7 @@ if __name__=='__main__':
                 i += 2
             if valid:
                 headings = ['ID', 'Type', 'Founder']
-                query_result = database.query_bloodline(query_args,query_arg_vals)
+                query_result = data.query_bloodline(query_args,query_arg_vals)
                 table_print(query_result,headings)
 
         elif words[0]=='bloodline_members':
@@ -276,7 +361,7 @@ if __name__=='__main__':
                 print('ERROR bloodline_members takes one argument (the id of the bloodline) and one argument only')
             else:
                 headings = ['Name', 'Dynasty']
-                query_result = database.query_bloodline_members(words[1])
+                query_result = data.query_bloodline_members(words[1])
                 table_print(query_result,headings)
 
         elif words[0]=="tree":
@@ -294,7 +379,7 @@ if __name__=='__main__':
                 continue
             
             args = " ".join(words[2:])
-            info, dag = database.descendant_tree(args)
+            info, dag = data.descendant_tree(args)
 
             if(isinstance(info, list)):
                 if(len(info) > 1):
