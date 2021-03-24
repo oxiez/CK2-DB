513699b3a3551cf6f348461d2ac15b494847c740
OwenDurkin <owen@thedurks.com>
2021-03-24 16:52:06 -0400

    user feedback on number of queries retrieved; if queries are truncated

--- commands/command.py
+++ commands/command.py
@@ -1,6 +1,6 @@
 import texttable
 
-row_count = 10
+row_count = 20
 
 # helper function for printing tables of data
 def table_print(query_result,headings=None):
@@ -8,13 +8,20 @@ def table_print(query_result,headings=None):
     if headings!=None:
         table.header(headings)
         table.set_max_width(210)
+    trunc_flag = False
     for i,v in enumerate(query_result):
-        if i >= row_count: break
+        if i >= row_count:
+            trunc_flag=True
+            break
         row = []
         row = row + [str(x) for x in v]
         table.add_row(row)
     t = table.draw()
     print(t)
+    if trunc_flag:
+        print("Results truncated to %i rows." % row_count,
+        "To see more or less, modify the number of rows to retreive with the `num_results` command.", sep="\n")
+    print("%i rows retreived." % len(query_result))
 
 
 class Command:
